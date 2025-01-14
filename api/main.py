87d0dc54d7637aedefeae750c854c298bcc61b3b
ovidiu.pascal <ovidiu.pascal@orange.com>
2025-01-14 10:43:55 +0100

    API -> Setting up the api (v.2)
    Signed-off-by: ovidiu.pascal <ovidiu.pascal@orange.com>

--- api/main.py
+++ api/main.py
@@ -10,64 +10,76 @@ from typing import Tuple, Type, Any, Dict, Optional, List, Union
 import mlflow.pyfunc
 from werkzeug.exceptions import BadRequest  # Import BadRequest exception
 
-# Names
-model_name = "2025-01-11 - LGBMClassifier - business.joblib"
-scaler_name = "2025-01-11 - RobustScaler.joblib"
-dataset_name = "04_prediction_df.csv"
+# ==================================================================================================================== #
+#                                                     CONFIGURATION                                                    #
+# ==================================================================================================================== #
+# MLflow Tracking URI
+MLFLOW_TRACKING_URI = "http://127.0.0.1:5000"
+mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 
-# Paths
-log_path = Path(LOG_DIR / "api")
-model_path = Path(MODEL_DIR / model_name)
-scaler_path = Path(MODEL_DIR / scaler_name)
-dataset_path = Path(PROCESSED_DATA_DIR / dataset_name)
+# Model Details
+MODEL_NAME = 'LGBMClassifier - business'
+MODEL_VERSION = None  # Use "None" for the latest version or specify an integer version
+
+# Dataset Path
+DATASET_NAME = "04_prediction_df.csv"
+DATASET_PATH = Path(PROCESSED_DATA_DIR / DATASET_NAME)
+
+if not DATASET_PATH .exists():
+    logger.error(f"Dataset file not found at {DATASET_PATH }")
+    raise FileNotFoundError(f"Dataset file not found at {DATASET_PATH }")
 
 # Loguru Configuration
-logger.add(log_path, rotation="1 MB", retention="7 days", level="INFO")
+LOG_PATH = Path(LOG_DIR / "api")
+logger.add(LOG_PATH, rotation="1 MB", retention="7 days", level="INFO")
 
 
-# Checks
-if not model_path.exists():
-    logger.error(f"Model file not found at {model_path}")
-    raise FileNotFoundError(f"Model file not found at {model_path}")
 
-if not scaler_path.exists():
-    logger.error(f"Scaler file not found at {scaler_path}")
-    raise FileNotFoundError(f"Scaler file not found at {scaler_path}")
+# Scaler
+SCALER_NAME = "2025-01-11 - RobustScaler.joblib"
+SCALER_PATH = Path(MODEL_DIR / SCALER_NAME)
 
-if not dataset_path.exists():
-    logger.error(f"Dataset file not found at {dataset_path}")
-    raise FileNotFoundError(f"Dataset file not found at {dataset_path}")
 
-# Loading Model
+# ==================================================================================================================== #
+#                                            LOADING RESOURCES FROM MLFLOW                                             #
+# ==================================================================================================================== #
+# Load Model from MLflow Model Registry
 try:
-    model = joblib.load(model_path)
-    # Additional verification step
-    if not model:
-        raise ValueError("Failed to load the model. The loaded object is invalid.")
-    logger.success("Model loaded successfully.")
+    if MODEL_VERSION:
+        model_uri = f"models:/{MODEL_NAME}/{MODEL_VERSION}"
+    else:
+        model_uri = f"models:/{MODEL_NAME}/latest"
+
+    model = mlflow.pyfunc.load_model(model_uri)
+    logger.success(f"Model loaded successfully from MLflow Model Registry: {model_uri}")
 except Exception as e:
-    logger.error(f"Error loading model: {e}")
-    raise RuntimeError("An error occurred while loading the model. Please check the logs for more details.")
+    logger.error(f"Error loading model from MLflow: {e}")
+    raise RuntimeError("An error occurred while loading the model from MLflow. Please check the logs.")
 
-# Loading Scaler
+# Load Scaler from MLflow Artifacts
 try:
+    with mlflow.start_run() as run:
+        # Get the scaler artifact URI from the model's run
+        scaler_path = mlflow.artifacts.download_artifacts(artifact_path="scalers/2025-01-14 - RobustScaler.joblib", run_id=run.info.run_id)
+
     robust_scaler = joblib.load(scaler_path)
-    if not robust_scaler:
-        raise ValueError("Failed to load the scaler. The loaded object is invalid.")
-    logger.success("Scaler loaded successfully.")
+    logger.success(f"Scaler loaded successfully from MLflow artifacts: {scaler_path}")
 except Exception as e:
-    logger.error(f"Error loading scaler: {e}")
-    raise RuntimeError("An error occurred while loading the scaler. Please check the logs for more details.")
+    logger.error(f"Error loading scaler from MLflow: {e}")
+    raise RuntimeError("An error occurred while loading the scaler from MLflow. Please check the logs.")
 
 
-# Loading Dataset
+# ==================================================================================================================== #
+#                                                    LOADING DATASET                                                   #
+# ==================================================================================================================== #
 try:
-    dataset = pd.read_csv(dataset_path)  # Load the CSV into a DataFrame
-    logger.success(f"Dataset loaded successfully from {dataset_path}")
+    dataset = pd.read_csv(DATASET_PATH )  # Load the CSV into a DataFrame
+    logger.success(f"Dataset loaded successfully from {DATASET_PATH}")
 except Exception as e:
     logger.error(f"Error loading dataset: {e}")
     raise RuntimeError("An error occurred while loading the dataset. Please check the logs for more details.")
 
+# =================================================== PREPROCESSING ================================================== #
 # Apply RobustScaler with PassThrough
 try:
     # Separate numeric and passthrough features
@@ -88,11 +100,15 @@ try:
     dataset_scaled = dataset_scaled[dataset.columns]  # Reorder to match the original structure
 
     logger.success("Applied RobustScaler to numeric features successfully.")
-
 except Exception as e:
     logger.error(f"Error applying RobustScaler: {e}")
     raise RuntimeError("An error occurred while scaling the dataset. Please check the logs for more details.")
 
+
+
+# ==================================================================================================================== #
+#                                                      DEBUG CHECK                                                     #
+# ==================================================================================================================== #
 # Log predicted probabilities for the first 1000 rows
 try:
     logger.info("Calculating predicted probabilities for the first 50 rows...")
@@ -113,13 +129,15 @@ except Exception as e:
 
 
 
-# ========== API ==========
+# ==================================================================================================================== #
+#                                                          API                                                         #
+# ==================================================================================================================== #
 app = Flask(__name__)
 
 @app.route("/predict", methods=["POST"])
 def predict():
     """
-    Endpoint to predict using the preloaded model for a given SK_ID_CURR.
+    Endpoint to predict using the model loaded from MLflow for a given SK_ID_CURR.
     Request Body Example:
     {
         "id": 100001
@@ -136,19 +154,19 @@ def predict():
             return jsonify({"error": "Missing 'id' in request payload"}), 400
 
         # Ensure both user_id and SK_ID_CURR are strings for comparison
-        user_id = str(user_request["id"])  # Convert input to string
-        dataset["SK_ID_CURR"] = dataset["SK_ID_CURR"].astype(str)  # Convert column to string
+        user_id = str(user_request["id"])                           # Convert input to string
+        dataset["SK_ID_CURR"] = dataset["SK_ID_CURR"].astype(str)   # Convert column to string
 
         # Check if ID exists in the dataset
         if user_id not in dataset["SK_ID_CURR"].values:
             return jsonify({"error": f"ID {user_id} not found in the dataset"}), 404
 
         # Select the row for the given ID, excluding SK_ID_CURR
-        user_data = dataset[dataset["SK_ID_CURR"] == user_id].drop(columns=["SK_ID_CURR"])
+        user_data = dataset_scaled[dataset_scaled["SK_ID_CURR"] == user_id].drop(columns=["SK_ID_CURR"])
 
         # Predict using the loaded model
-        proba = model.predict_proba(user_data)[:, 1][0]  # Get the probability of the positive class
-        prediction = 1 if proba >= 0.46 else 0          # Apply the custom threshold
+        proba = model.predict(user_data).iloc[0]  # Predict method for MLflow model
+        prediction = 1 if proba >= 0.46 else 0  # Apply custom threshold
 
         # Define status based on prediction
         status = "Granted" if prediction == 0 else "Denied"
@@ -166,10 +184,6 @@ def predict():
     except Exception as e:
         logger.error(f"Error processing prediction: {e}")
         return jsonify({"error": "An error occurred while processing the prediction request"}), 500
-
-
-
-
 
 
 if __name__ == "__main__":
